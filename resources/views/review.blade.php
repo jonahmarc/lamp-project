5c2fcd378518943d8f0a9b356a0c546b6ce8e9b2
jonahmarc <olympusfoxtrot@gmail.com>
2024-02-01 13:22:56 +0800

    used tailwind for styling

--- resources/views/review.blade.php
+++ resources/views/review.blade.php
@@ -3,44 +3,58 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="{{asset ('assets/css/styles.css')}}">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <!-- <link rel="stylesheet" href="{{asset ('assets/css/styles.css')}}"> -->
     <title>Customer Information Review Page</title>
 </head>
-<body>
-    <div class="header">
-        <a class="form-page" href="/">CUSTOMER INFORMATION ENTRY FORM</a>
-        <a class="review-page" href="/review">CUSTOMER INFORMATION REVIEW PAGE</a>
+<body class="grid h-screen w-screen m-0 grid-rows-8 bg-cyan-100">
+    <div class="row-span-1 flex w-full justify-end items-center bg-cyan-900 shadow-xl text-cyan-100">
+        <a class="form-page hover:underline" href="/">CUSTOMER INFORMATION ENTRY FORM</a>
+        <a class="review-page ml-4 hover:underline" href="/review">CUSTOMER INFORMATION REVIEW PAGE</a>
     </div>
-    <div class="page-container">
-        <div class="review-wrapper">
-            <form action="/info" method="GET" class="email-lookup">
-                <!-- @csrf -->
-                <div class="input-wrapper">
-                    <label for="email">Email: </label>
-                    <input type="email" name="email" id="email" required/>
-                    @error('email')
-                        <p style="color: red; font-size: 0.5em; margin: 0;">
-                            {{$message}}
-                        </p>
-                    @enderror
-                    <button type="submit">SEARCH</button>
+    <div class="form-wrapper row-span-7 flex flex-wrap pt-10">
+        <div class="flex-initial w-1/2 grid grid-rows-8">
+            <form  action="/info" method="GET"class="row-span-2 flex flex-wrap px-10 justify-center content-center">
+                <div class="grid content-center">
+                    <p class="text-cyan-900 font-bold align-middle">Email</p>
                 </div>
-            </form>
-            <div class="result">
-                @foreach ($customer as $key => $info)
-                    <div class="input-wrapper">
-                        @if ($key == 'error')
-                            <label for="" style="text-transform: capitalize; color: red; font-size: 0.5em; margin: 0;"><strong>{{ $key }}:</strong></label>
-                            <label for="" style="color: red; font-size: 0.5em; margin: 0;">{{ $info }}</label>
-                        @else
-                            <label for="" style="text-transform: capitalize;"><strong>{{ $key }}:</strong></label>
-                            <label for="">{{ $info }}</label>
-                        @endif
+                <input class="bg-white appearance-none border-2 border-cyan-700 rounded w-1/3 h-1/4 py-2 px-4 mx-2 text-gray-700 leading-tight focus:outline-none focus:border-cyan-400" name="email" id="inline-email" type="email" value="">
+                <button type="submit" class="shadow bg-cyan-900 w-1/8 h-1/4 hover:bg-cyan-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded">
+                    SEARCH
+                </button>
+                @error('email')
+                    <div class="grid content-center w-full">
+                        <p class="text-red-600 font-bold align-middle">{{ $message }}</p>
                     </div>
-                @endforeach
+                @enderror
+            </form>
+            <div class="row-span-6 flex justify-center">
+                <div class="w-4/12">
+                    @foreach ($customer as $key => $info)
+                        @if ($key == 'error')
+                            <div class="flex flex-wrap justify-between content-start">
+                                <div class="grid content-center mr-2">
+                                    <p class="uppercase italic text-red-600 font-bold align-middle">{{ $key }}</p>
+                                </div>
+                                <div class="grid content-center">
+                                    <p class="text-red-600 font-bold align-middle">{{ $info }}</p>
+                                </div>
+                            </div>
+                        @else
+                            <div class="flex flex-wrap justify-between content-start">
+                                <div class="grid content-start mr-2">
+                                    <p class="capitalize italic text-cyan-900 font-bold align-middle">{{ $key }}</p>
+                                </div>
+                                <div class="grid content-start">
+                                    <p class="text-cyan-900 align-middle">{{ $info }}</p>
+                                </div>
+                            </div>
+                        @endif
+                    @endforeach
+                </div>
             </div>
         </div>
-        <div class="calculator">
+        <div class="flex flex-initial w-1/2 justify-center">
             <iframe src="calculator.blade.php" frameborder="0"></iframe>
         </div>
     </div>
